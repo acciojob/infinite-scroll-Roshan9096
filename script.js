858f05c7514c5e60e15c82d65d806e6d4ce9cb4a
Roshan9096 <157248010+Roshan9096@users.noreply.github.com>
2025-11-03 18:56:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here!
 
+let id = document.getElementById("infi-list")
+let count =1;
+for(let i=0;i<10;i++){
+	let ol = document.createElement("li")
+	ol.textContent = `Item ${count}`
+	count++;
+	id.appendChild(ol)
+}
+
+id.addEventListener("scroll",()=>{
+  for(let i=0;i<2;i++){
+	let ol = document.createElement("li")
+	ol.textContent = `Item ${count}`
+	count++;
+	id.appendChild(ol)
+}
+})
